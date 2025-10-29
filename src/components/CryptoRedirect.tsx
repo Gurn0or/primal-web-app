0c8340ae0c4202430a7a7d6ea7f8cd1b838a6021
Gurnoor <68712145+Gurn0or@users.noreply.github.com>
2025-10-30 01:02:37 +0530

    Add static CryptoRedirect component
    Create CryptoRedirect component that uses useEffect to redirect /Cryptonomicon to the target profile URL using window.location.replace

---
+++ src/components/CryptoRedirect.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+const CryptoRedirect = () => {
+  useEffect(() => {
+    window.location.replace('/p/nprofile1qqsxzyttdge3z04sunl2984zhj230gahmqs37pcw5p8zxdwxdfs772cw3pjtq');
+  }, []);
+
+  return null;
+};
+
+export default CryptoRedirect;
