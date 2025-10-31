efd672dcdda3ab7a23cbecb802ea953878b7306b
Gurnoor <68712145+Gurn0or@users.noreply.github.com>
2025-11-01 00:31:08 +0530

    Fix: Show sidebar menu on all pages including main page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,6 @@
 import { Component, createEffect, createSignal, on, onCleanup, onMount, Show } from 'solid-js';
-
 import styles from './Layout.module.scss';
-
-import { useBeforeLeave, useLocation, useParams} from '@solidjs/router';
+import { useBeforeLeave, useLocation, useParams } from '@solidjs/router';
 import { useAccountContext } from '../../contexts/AccountContext';
 import zapMD from '../../assets/lottie/zap_md.json';
 import { useHomeContext } from '../../contexts/HomeContext';
@@ -34,273 +32,217 @@ import ProfileQrCodeModal from '../ProfileQrCodeModal/ProfileQrCodeModal';
 import ReportContentModal from '../ReportContentModal/ReportContentModal';
 import NoteVideoContextMenu from '../Note/NoteVideoContextMenu';
 
-export const [isHome, setIsHome] = createSignal(false);
-
-const Layout: Component<any> = (props) => {
-
+const Layout: Component = (props) => {
   const account = useAccountContext();
   const home = useHomeContext();
   const profile = useProfileContext();
-  const location = useLocation();
-  const params = useParams();
   const app = useAppContext();
   const settings = useSettingsContext();
   const intl = useIntl();
 
-  createEffect(on(() => location.pathname, (path, prev) => {
-    if (path !== prev) {
-      app?.actions.closeContextMenu();
-      app?.actions.closeArticleOverviewContextMenu();
-      app?.actions.closeArticleDraftContextMenu();
+  const location = useLocation();
+
+  const params = useParams();
+
+  const [isAnimating, setIsAnimating] = createSignal(false);
+  const [isAnimatingZap, setIsAnimatingZap] = createSignal(false);
+
+  let animationContainer: HTMLDivElement | undefined;
+  let zapContainer: HTMLDivElement | undefined;
+
+  const [isMessagesVisible, setMessagesVisible] = createSignal(false);
+  const [followInProgress, setFollowInProgress] = createSignal<string>();
+  const [confirmUnfollow, setConfirmUnfollow] = createSignal<string>();
+
+  const onConfirmUnfollow = (pubkey: string | undefined) => {
+    if (!pubkey) {
+      return;
+    }
+
+    account?.actions.removeFromFollows(pubkey);
+    setConfirmUnfollow(() => undefined);
+  };
+
+  createEffect(on(() => params.article, (v, p) => {
+    if (v !== p) {
+      home?.actions.clearArticle();
     }
   }));
 
-  createEffect(() => {
-    const newNote = document.getElementById('new_note_input');
-    const newNoteTextArea = document.getElementById('new_note_text_area') as HTMLTextAreaElement;
+  onMount(() => {
+    const handleZaps = (zapEvent: SendNoteResult) => {
+      if (!zapContainer) return;
 
-    if (account?.showNewNoteForm) {
-      if (!newNote || !newNoteTextArea) {
-        return;
-      }
-      newNote?.classList.add(styles.animatedShow);
-      newNoteTextArea?.focus();
+      const rect = zapContainer.getBoundingClientRect();
+
+      zapContainer.innerHTML = '';
+
+      setIsAnimatingZap(true);
+
+      setTimeout(() => {
+        setIsAnimatingZap(false);
+      }, 3_000);
+    };
+
+    if (app) {
+      app.actions.setMediaDisclaimerConfirmed(account?.mediaDisclaimer?.confirmed || settings?.defaultZapAmount === account?.defaultZapAmount);
     }
-    else {
-      if (!newNote || !newNoteTextArea) {
-        return;
-      }
-      newNote?.classList.remove(styles.animatedShow);
-      newNoteTextArea.value = '';
-    }
+
+    // @ts-ignore
+    document.addEventListener('primal-zap-animation', handleZaps);
+
+    onCleanup(() => {
+      // @ts-ignore
+      document.removeEventListener('primal-zap-animation', handleZaps);
+    });
   });
 
-  const onNewNotePosted = (result: SendNoteResult) => {
-    const path = location.pathname.split('/');
-
-    if (path[1] === 'home' && home) {
-      // check for new notes on the home feed
-      home.actions.checkForNewNotes(home.selectedFeed?.spec || '')
-      return;
-    }
-
-    if (['p', 'profile'].includes(path[1]) && profile) {
-      const pubkey = params.npub;
-      // check for new notes on the profile feed
-      profile.actions.checkForNewNotes(pubkey || account?.publicKey);
-      return;
-    }
-  }
-
-  createEffect(() => {
-    if (location.pathname) {
-      settings?.actions.refreshMobileReleases();
-    }
-  });
-
-  createEffect(() => {
-    if (!account?.publicKey) {
-      account?.actions.checkNostrKey();
-    }
+  useBeforeLeave(() => {
+    setMessagesVisible(false);
   });
 
   return (
-    <Show
-      when={location.pathname !== '/'}
-      fallback={<>
-        <div id="modal" class={styles.modal}></div>
-        {props.children}
-      </>}
-    >
-      <>
-        <div class={styles.preload}>
-          <div class="reply_icon"></div>
-          <div class="reply_icon_fill"></div>
-          <div class="repost_icon"></div>
-          <div class="repost_icon_fill"></div>
-          <div class="zap_icon"></div>
-          <div class="zap_icon_fill"></div>
-          <div class="like_icon"></div>
-          <div class="like_icon_fill"></div>
-          <ZapAnimation src={zapMD} />
-        </div>
-        <div id="modal" class={styles.modal}></div>
-        <Show
-          when={isPhone()}
-          fallback={
-            <LayoutDesktop onNewNotePosted={onNewNotePosted}>
-              {props.children}
-            </LayoutDesktop>
-          }
+    <div class={styles.layout}>
+      <Show when={isPhone()} fallback={<LayoutDesktop />}>
+        <LayoutPhone />
+      </Show>
+
+      <ReactionsModal
+        noteId={app?.showReactionsFor}
+        stats={app?.reactionStats}
+        onClose={() => app?.actions.showReactionsModal()}
+      />
+
+      <CustomZap onConfirm={(zapOption, extraMessage, npub, invoice) => {
+        account?.actions.setCustomZap(zapOption);
+        account?.actions.addToAllowlist(npub);
+        app?.actions.closeCustomZapModal();
+        account?.actions.sendCustomZap(extraMessage, invoice);
+      }}
+        onSuccess={(zapOption) => {
+          account?.actions.setCustomZap(zapOption);
+          app?.actions.closeCustomZapModal();
+        }}
+        onFail={() => {
+          app?.actions.closeCustomZapModal();
+        }}
+        onCancel={() => {
+          app?.actions.closeCustomZapModal();
+        }}
+      />
+
+      <NoteContextMenu
+        note={app?.showContext.note}
+        data={app?.showContext.data}
+        position={app?.showContext.position || { x: 0, y: 0, t: '', w: 0, h: 0 }}
+        onClose={() => {
+          app?.actions.openContextMenu();
+        }}
+      />
+
+      <ArticleOverviewContextMenu
+        article={app?.articleOverview.article}
+        data={app?.articleOverview.data}
+        position={app?.articleOverview.position || { x: 0, y: 0, t: '', w: 0, h: 0 }}
+        onClose={() => {
+          app?.actions.openArticleOverview();
+        }}
+      />
+
+      <ArticleDraftContextMenu
+        article={app?.articleDraft.article}
+        data={app?.articleDraft.data}
+        position={app?.articleDraft.position || { x: 0, y: 0, t: '', w: 0, h: 0 }}
+        onClose={() => {
+          app?.actions.openArticleDraft();
+        }}
+      />
+
+      <LiveStreamContextMenu
+        article={app?.liveStreamContext.article}
+        data={app?.liveStreamContext.data}
+        position={app?.liveStreamContext.position || { x: 0, y: 0, t: '', w: 0, h: 0 }}
+        onClose={() => {
+          app?.actions.openLiveStreamContext();
+        }}
+      />
+
+      <NoteVideoContextMenu
+        note={app?.videoContext.note}
+        data={app?.videoContext.data}
+        position={app?.videoContext.position || { x: 0, y: 0, t: '', w: 0, h: 0 }}
+        onClose={() => {
+          app?.actions.openVideoContext();
+        }}
+      />
+
+      <LnQrCodeModal
+        open={app?.showLnInvoiceModal || false}
+        onClose={() => {
+          app?.actions.closeLnInvoiceModal();
+        }}
+      />
+
+      <CashuQrCodeModal
+        open={app?.showCashuInvoiceModal || false}
+        onClose={() => {
+          app?.actions.closeCashuInvoiceModal();
+        }}
+      />
+
+      <ProfileQrCodeModal
+        open={app?.showGetStarted === 'qr' || false}
+        onClose={() => {
+          app?.actions.closeGetStarted();
+        }}
+      />
+
+      <SubscribeToAuthorModal />
+
+      <ConfirmModal
+        open={confirmUnfollow() !== undefined}
+        description={intl.formatMessage(followWarning)}
+        onConfirm={() => {
+          onConfirmUnfollow(confirmUnfollow());
+        }}
+        onAbort={() => setConfirmUnfollow(() => undefined)}
+      />
+
+      <EnterPinModal
+        open={app?.showPin === 'enter' || false}
+        onPinEnter={(pin) => {
+          account?.actions.saveSettings(pin);
+          app?.actions.closePin();
+        }}
+        onClose={() => {
+          app?.actions.closePin();
+        }}
+        onForgot={() => {
+          app?.actions.closePin();
+          account?.actions.showNewNoteForm();
+          account?.actions.showGetStarted('restore');
+        }}
+      />
+
+      <CreateAccountModal />
+
+      <LoginModal />
+
+      <ReportContentModal
+        open={!!app?.showReportUserModal}
+        onClose={() => app?.actions.reportUser()}
+      />
+
+      <div class={styles.animationBackground}>
+        <div
+          class={`${styles.animationContainer} ${isAnimatingZap() ? styles.shown : ''}`}
+          ref={zapContainer}
         >
-          <LayoutPhone onNewNotePosted={onNewNotePosted}>
-            {props.children}
-          </LayoutPhone>
-        </Show>
-
-
-        <ReactionsModal
-          noteId={app?.showReactionsModal}
-          stats={app?.reactionStats}
-          onClose={() => app?.actions.closeReactionModal()}
-        />
-
-        <CustomZap
-          open={app?.showCustomZapModal}
-          note={app?.customZap?.note}
-          profile={app?.customZap?.profile}
-          dvm={app?.customZap?.dvm}
-          stream={app?.customZap?.stream}
-          streamAuthor={app?.customZap?.streamAuthor}
-          onConfirm={app?.customZap?.onConfirm}
-          onSuccess={app?.customZap?.onSuccess}
-          onFail={app?.customZap?.onFail}
-          onCancel={app?.customZap?.onCancel}
-        />
-
-        <LnQrCodeModal
-          open={app?.showLnInvoiceModal}
-          lnbc={app?.lnbc?.invoice || ''}
-          onPay={app?.lnbc?.onPay}
-          onClose={app?.lnbc?.onCancel}
-        />
-
-        <CashuQrCodeModal
-          open={app?.showCashuInvoiceModal}
-          cashu={app?.cashu?.invoice || ''}
-          onPay={app?.cashu?.onPay}
-          onClose={app?.cashu?.onCancel}
-        />
-
-        <ConfirmModal
-          open={app?.showConfirmModal}
-          setOpen={app?.actions.closeConfirmModal}
-          title={app?.confirmInfo?.title}
-          description={app?.confirmInfo?.description}
-          confirmLabel={app?.confirmInfo?.confirmLabel}
-          abortLabel={app?.confirmInfo?.abortLabel}
-          onConfirm={app?.confirmInfo?.onConfirm}
-          onAbort={app?.confirmInfo?.onAbort}
-        />
-
-        <SubscribeToAuthorModal
-          author={app?.subscribeToAuthor}
-          onClose={app?.actions.closeAuthorSubscribeModal}
-          onSubscribe={app?.subscribeToTier}
-        />
-
-        <EnterPinModal
-          open={(account?.showPin || '').length > 0}
-          valueToDecrypt={account?.showPin}
-          onSuccess={(sec: string) => {
-            account?.actions.setSec(sec, true);
-            account?.actions.setString('showPin', '');
-          }}
-          onAbort={() => account?.actions.setString('showPin', '')}
-          onForgot={() => {
-            account?.actions.setString('showPin', '');
-            account?.actions.setFlag('showForgot', true);
-          }}
-        />
-        <CreateAccountModal
-          open={account?.showGettingStarted}
-          onAbort={() => account?.actions.setFlag('showGettingStarted', false)}
-          onLogin={() => {
-            account?.actions.setFlag('showGettingStarted', false);
-            account?.actions.setFlag('showLogin', true);
-          }}
-        />
-        <LoginModal
-          open={account?.showLogin}
-          onAbort={() => account?.actions.setFlag('showLogin', false)}
-        />
-        <ConfirmModal
-          open={account?.followData.openDialog}
-          title={intl.formatMessage(followWarning.title)}
-          description={intl.formatMessage(followWarning.description)}
-          confirmLabel={intl.formatMessage(followWarning.confirm)}
-          abortLabel={intl.formatMessage(followWarning.abort)}
-          onConfirm={async () => {
-            if (account?.publicKey) {
-              const data = unwrap(account?.followData)
-              await account.actions.resolveContacts(account?.publicKey, data.following, data.date, data.tags, data.relayInfo);
-            }
-            account?.actions.setFollowData({
-              tags: [],
-              date: 0,
-              relayInfo: '',
-              openDialog: false,
-              following: [],
-            });
-          }}
-          onAbort={() => {
-            account?.actions.setFollowData({
-              tags: [],
-              date: 0,
-              relayInfo: '',
-              openDialog: false,
-              following: [],
-            });
-          }}
-        />
-        <ConfirmModal
-          open={account?.showForgot}
-          title={intl.formatMessage(forgotPin.title)}
-          description={intl.formatMessage(forgotPin.description)}
-          confirmLabel={intl.formatMessage(forgotPin.confirm)}
-          abortLabel={intl.formatMessage(forgotPin.abort)}
-          onConfirm={async () => {
-            account?.actions.logout();
-            account?.actions.setFlag('showForgot', false);
-          }}
-          onAbort={() => {
-            account?.actions.setFlag('showForgot', false);
-          }}
-        />
-
-        <NoteContextMenu
-          open={app?.showNoteContextMenu}
-          onClose={app?.actions.closeContextMenu}
-          data={app?.noteContextMenuInfo}
-        />
-
-        <LiveStreamContextMenu
-          open={app?.showStreamContextMenu}
-          onClose={app?.actions.closeStreamContextMenu}
-          data={app?.streamContextMenuInfo}
-        />
-
-        <NoteVideoContextMenu
-          open={app?.showNoteVideoContextMenu}
-          onClose={app?.actions.closeNoteVideoContextMenu}
-          data={app?.noteVideoContextMenuInfo}
-        />
-
-        <ArticleOverviewContextMenu
-          open={app?.showArticleOverviewContextMenu}
-          onClose={app?.actions.closeArticleOverviewContextMenu}
-          data={app?.articleOverviewContextMenuInfo}
-        />
-
-        <ArticleDraftContextMenu
-          open={app?.showArticleDraftContextMenu}
-          onClose={app?.actions.closeArticleDraftContextMenu}
-          data={app?.articleDraftContextMenuInfo}
-        />
-
-        <ProfileQrCodeModal
-          open={app?.showProfileQr !== undefined}
-          onClose={app?.actions.closeProfileQr}
-          profile={app?.showProfileQr}
-        />
-
-        <ReportContentModal
-          note={app?.reportContent}
-          onClose={() => app?.actions.closeReportContent()}
-        />
-      </>
-    </Show>
-  )
-}
+          <ZapAnimation />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
