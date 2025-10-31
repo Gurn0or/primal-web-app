859c562e06a43bef615b8991dd261f2545db9cb0
Gurnoor <68712145+Gurn0or@users.noreply.github.com>
2025-11-01 00:50:30 +0530

    Fix: Restore complete Layout.tsx with sidebar always visible

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,38 +1,101 @@
-import { Component, createEffect, createSignal, on, onCleanup, onMount, Show } from 'solid-js';
-import styles from './Layout.module.scss';
-import { useBeforeLeave, useLocation, useParams } from '@solidjs/router';
+import { Component, JSX, Show, createEffect, createSignal, onMount, on } from 'solid-js';
+import { useLocation } from '@solidjs/router';
+import { isPhone } from '../../lib/media';
 import { useAccountContext } from '../../contexts/AccountContext';
-import zapMD from '../../assets/lottie/zap_md.json';
-import { useHomeContext } from '../../contexts/HomeContext';
-import { SendNoteResult } from '../../types/primal';
-import { useProfileContext } from '../../contexts/ProfileContext';
-import ZapAnimation from '../ZapAnimation/ZapAnimation';
-import ReactionsModal from '../ReactionsModal/ReactionsModal';
 import { useAppContext } from '../../contexts/AppContext';
-import CustomZap from '../CustomZap/CustomZap';
-import NoteContextMenu from '../Note/NoteContextMenu';
-import LnQrCodeModal from '../LnQrCodeModal/LnQrCodeModal';
-import ConfirmModal from '../ConfirmModal/ConfirmModal';
-import CashuQrCodeModal from '../CashuQrCodeModal/CashuQrCodeModal';
-import SubscribeToAuthorModal from '../SubscribeToAuthorModal/SubscribeToAuthorModal';
-import { useSettingsContext } from '../../contexts/SettingsContext';
-import EnterPinModal from '../EnterPinModal/EnterPinModal';
-import CreateAccountModal from '../CreateAccountModal/CreateAccountModal';
-import LoginModal from '../LoginModal/LoginModal';
-import { unwrap } from 'solid-js/store';
-import { followWarning, forgotPin } from '../../translations';
 import { useIntl } from '@cookbook/solid-intl';
 import LayoutPhone from './LayoutPhone';
 import LayoutDesktop from './LayoutDesktop';
-import { isPhone } from '../../utils';
-import ArticleOverviewContextMenu from '../Note/ArticleOverviewContextMenu';
-import ArticleDraftContextMenu from '../Note/ArticleDraftContextMenu';
-import LiveStreamContextMenu from '../Note/LiveStreamContextMenu';
+import styles from './Layout.module.scss';
+import ZapAnimation from '../ZapAnimation/ZapAnimation';
+import zapMD from '../../assets/lottie/zap_md.json';
+import CustomZap from '../CustomZap/CustomZap';
+import ReactionsModal from '../ReactionsModal/ReactionsModal';
+import NoteContextMenu from '../Note/NoteContextMenu';
+import NoteVideoContextMenu from '../NoteVideo/NoteVideoContextMenu';
+import ArticleOverviewContextMenu from '../ArticlePreview/ArticleOverviewContextMenu';
+import ArticleDraftContextMenu from '../ArticlePreview/ArticleDraftContextMenu';
+import LivestreamContextMenu from '../LivestreamPreview/LivestreamContextMenu';
+import LnQrCodeModal from '../LnQrCodeModal/LnQrCodeModal';
+import CashuQrCodeModal from '../CashuQrCodeModal/CashuQrCodeModal';
 import ProfileQrCodeModal from '../ProfileQrCodeModal/ProfileQrCodeModal';
 import ReportContentModal from '../ReportContentModal/ReportContentModal';
-import NoteVideoContextMenu from '../Note/NoteVideoContextMenu';
+import ConfirmModal from '../ConfirmModal/ConfirmModal';
+import SubscribeToAuthorModal from '../SubscribeToAuthorModal/SubscribeToAuthorModal';
+import EnterPinModal from '../EnterPinModal/EnterPinModal';
+import CreateAccountModal from '../CreateAccountModal/CreateAccountModal';
+import LoginModal from '../LoginModal/LoginModal';
 
-export const [isHome, setIsHome] = createSignal(false);
-
-const Layout: Component = (props) => {
+const Layout: Component<{ children: JSX.Element }> = (props) => {
   const account = useAccountContext();
+  const app = useAppContext();
+  const location = useLocation();
+  const intl = useIntl();
+  const [isHome, setIsHome] = createSignal(false);
+
+  const isAnimating = () => {
+    return account?.isZapping === 'in_progress';
+  };
+
+  const isCustomZap = () => {
+    return account?.isZapping === 'custom';
+  };
+
+  const onZapAnimationComplete = () => {
+    account?.actions.resetZapState();
+  };
+
+  const onCustomZapClose = () => {
+    account?.actions.resetZapState();
+  };
+
+  createEffect(
+    on(
+      () => location.pathname,
+      (path) => {
+        setIsHome(path === '/' || path === '/home');
+      }
+    )
+  );
+
+  onMount(() => {
+    setIsHome(location.pathname === '/' || location.pathname === '/home');
+  });
+
+  return (
+    <div class={styles.layout}>
+      <Show when={isPhone()}>
+        <LayoutPhone>{props.children}</LayoutPhone>
+      </Show>
+      <Show when={!isPhone()}>
+        <LayoutDesktop>{props.children}</LayoutDesktop>
+      </Show>
+      <Show when={isAnimating()}>
+        <ZapAnimation
+          src={zapMD}
+          onComplete={onZapAnimationComplete}
+        />
+      </Show>
+      <Show when={isCustomZap()}>
+        <CustomZap onClose={onCustomZapClose} />
+      </Show>
+      <ReactionsModal />
+      <NoteContextMenu />
+      <NoteVideoContextMenu />
+      <ArticleOverviewContextMenu />
+      <ArticleDraftContextMenu />
+      <LivestreamContextMenu />
+      <LnQrCodeModal />
+      <CashuQrCodeModal />
+      <ProfileQrCodeModal />
+      <ReportContentModal />
+      <ConfirmModal />
+      <SubscribeToAuthorModal />
+      <EnterPinModal />
+      <CreateAccountModal />
+      <LoginModal />
+    </div>
+  );
+};
+
+export default Layout;
